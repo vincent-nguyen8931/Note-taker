7537f6e33b76298b4268a9eb3fe41c9fe78fc472
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-26 02:30:01 -0800

    Create post route in api routes.

--- public/assets/js/APIRoutes.js
+++ public/assets/js/APIRoutes.js
@@ -2,9 +2,22 @@ var express = require('express')
 var app = express()
 var fs = require("fs")
 
-app.get("/api/notes", function (req,res) {
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/api/notes", function (req, res) {
   fs.readFile("/db/db.json", function (err, data) {
     if (err) throw err;
     res.json(JSON.parse(data));
   })
 })
+
+app.post("/api/notes", function (req, res) {
+  var noteTitle = req.body.title;
+  var noteText = req.body.text;
+
+  fs.writeFile("/db/db.json", JSON.stringify(res.send(noteTitle + " " + noteText)), function (err) {
+    if (err) throw err;
+    console.log("write file successful");
+  })
+});
