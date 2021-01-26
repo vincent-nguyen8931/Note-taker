c2c1443047fd163fa18e17b45df799f34dc85a9d
Vincent Nguyen <vincent.nguyen8931@gmail.com>
2021-01-25 18:31:06 -0800

    Create notes api route.

--- public/assets/js/APIRoutes.js
+++ public/assets/js/APIRoutes.js
@@ -1,2 +1,10 @@
 var express = require('express')
 var app = express()
+var fs = require("fs")
+
+app.get("/api/notes", function (req,res) {
+  fs.readFile("/db/db.json", function (err, data) {
+    if (err) throw err;
+    res.json(JSON.parse(data));
+  })
+})
